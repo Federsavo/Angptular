41be5060e3f1f6fe85a63ab87d4accec2a093be9
dario <antoniazzidario26@gmail.com>
2025-01-13 18:01:54 +0100

    gg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {PippoNeiGuaiComponent} from './components/pippo-nei-guai/pippo-nei-guai.component';
+import {PaperopoliComponent} from './components/paperopoli/paperopoli.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PippoNeiGuaiComponent, PaperopoliComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   standalone:true
--- src/app/components/pippo-nei-guai/pippo-nei-guai.component.ts
+++ src/app/components/pippo-nei-guai/pippo-nei-guai.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-pippo-nei-guai',
   imports: [],
   templateUrl: './pippo-nei-guai.component.html',
+  standalone: true,
   styleUrl: './pippo-nei-guai.component.css'
 })
 export class PippoNeiGuaiComponent {
